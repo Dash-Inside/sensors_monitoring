393a635d9bdbcbbd7269842450620ecaf9f992c9
(No) Zorro Positivo <pocket.red.fox@gmail.com>
2023-10-11 19:17:57 +0300

    feat: add search func's to drawer controller

--- lib/src/presentation/controllers/monitoring_drawer_controller.dart
+++ lib/src/presentation/controllers/monitoring_drawer_controller.dart
@@ -2,14 +2,27 @@ import 'package:flutter/material.dart';
 
 class MonitoringDrawerController with ChangeNotifier {
   bool _isOpen = true;
-  bool get isOpen => _isOpen;
+  bool _isSearchActive = false;
 
+  bool get isOpen => _isOpen;
   set isOpen(bool value) {
     _isOpen = value;
     notifyListeners();
   }
 
-  void onDrawerButtonPressed() {
-    isOpen = !isOpen;
+  bool get isSearchActive => _isSearchActive;
+  set isSearchActive(bool value) {
+    if (value) {
+      _isOpen = true;
+      _isSearchActive = true;
+    } else {
+      _isSearchActive = false;
+    }
+
+    notifyListeners();
   }
+
+  void openSearchModule() => isSearchActive = true;
+  void closeSearchModule() => isSearchActive = false;
+  void onDrawerButtonPressed() => isOpen = !isOpen;
 }
